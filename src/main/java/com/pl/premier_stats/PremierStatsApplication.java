589e659a7f7546f5bd500a54c7f5163928bcfd70
Hamza <hamzayusuf26@outlook.com>
2024-09-26 08:02:36 +0100

    first commit

---
+++ src/main/java/com/pl/premier_stats/PremierStatsApplication.java
@@ -0,0 +1,13 @@
+package com.pl.premier_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierStatsApplication.class, args);
+	}
+
+}
