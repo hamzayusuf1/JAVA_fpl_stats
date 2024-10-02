d5c47d08647cb83a5d3d3581407d65c31c4f8918
Hamza <hamzayusuf26@outlook.com>
2024-10-02 10:45:59 +0100

    Added business log to service and created reqs in controller

---
+++ src/test/java/com/pl/premier_stats/PremierStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.premier_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
